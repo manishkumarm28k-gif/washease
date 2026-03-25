32943341a6a99eb6637f17f9ded995e4eb5623d1
manishkumarm28k-gif <manishkumar.m28k@gmail.com>
2026-03-26 03:04:36 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,13 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    # basic config
+    app.config['SECRET_KEY'] = 'secret123'
+
+    # simple test route (IMPORTANT)
+    @app.route("/")
+    def home():
+        return "WashEase Full App Running 🚀"
+
     return app
