eea169c6b5969489794f41f5658b9b41a704f368
manishkumarm28k-gif <manishkumar.m28k@gmail.com>
2026-03-26 02:33:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-import sys
-import os
+from flask import Flask
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+app = Flask(__name__)
 
-from app import create_app
-
-flask_app = create_app()
-
-if __name__ == '__main__':
-    flask_app.run(debug=True, port=5000)
+@app.route("/")
+def home():
+    return "App is running 🚀"
